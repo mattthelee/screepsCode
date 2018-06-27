afcce3dbb9fc0e48fba42615dcfb094a881a4057
Matthew Lee <matt_lee92@hotmail.co.uk>
2018-06-27 08:48:52 +0100

    adding plan for job based approach

---
+++ jobs.js
@@ -0,0 +1,5 @@
+// I want to generate a list of jobs that need doing and order them by priority
+// Resource extraction should be number 1 priority until storage is full
+// When storage full we want priority to switch to spending resources.
+// Full screeps should be assigned to one of the spending priorities.
+// priorities for screeps should be based on the the proximity of the job start. 
